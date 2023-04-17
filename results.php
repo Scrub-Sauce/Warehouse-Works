778d013e4f61526a3a2bc16e6491662887d62306
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-17 17:26:58 -0500

    Removed the manufacture column from the query performed by results.php

--- results.php
+++ results.php
@@ -4,16 +4,16 @@ $db = db_iconnect('warehouse-works');
 $time_start = microtime(true);
 
 $query = $_POST['manufacture'];
-$sql = "SELECT * FROM `equipment` WHERE `manufacture` = '$query'";
+$sql = "SELECT `type`, `serial_number` FROM `equipment` WHERE `manufacture` = '$query'";
 $result = $db->query($sql) or 
     die("Something went wrong with $sql<br>".$db->error);
-
+echo '<h3>Search by manufacture: '.$query.'</h3>';
 echo '<table>';
-echo '<tr><th>Type</th><th>Manufacture</th><th>Serial Number</th></tr>';
+echo '<tr><th>Type</th><th>Serial Number</th></tr>';
 while($data=$result->fetch_array(MYSQLI_ASSOC )){
     echo '<tr>';
         echo '<td>'.$data['type'].'</td>';
-        echo '<td>'.$data['manufacture'].'</td>';
+        // echo '<td>'.$data['manufacture'].'</td>';
         echo '<td>'.$data['serial_number'].'</td>';
     echo '</tr>';
 }
