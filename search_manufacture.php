4710a536a1f853ab7db168ec6c6c5014e161cf4b
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-18 00:39:35 -0500

    Updated search_manufacture.php

--- search_manufacture.php
+++ search_manufacture.php
@@ -13,62 +13,100 @@ if(isset($_POST['submit']) && ($_POST['submit'] == "submit"))
     $m_wild = true;
     $t_wild = true;
 
-    $manufacture = NULL;
+    $sql = "SELECT `auto_id`, `name` FROM `manufacture`";
+    $result = $db->query($sql);
+    $m_map = array();
+    foreach($result as $row){
+        $m_map[$row['auto_id']] = $row['name'];
+    }
+    
     if($m_query == '%'){
         $manufacture = 'Any';
     }else{
         $m_wild = false;
-        $sql = "SELECT `name` FROM `manufacture` WHERE `auto_id` = '$m_query'";
-        $result = $db->query($sql) or 
-            die("Something went wrong with $sql<br>".$db->error);
-
-        
-        while($data = $result->fetch_array(MYSQLI_ASSOC))
-        {
-            $manufacture = $data['name'];
-        }
+        $manufacture = $m_map[$m_query];
     }
-    
+
+    $sql = "SELECT `auto_id`, `name` FROM `type`";
+    $result = $db->query($sql);
+    $t_map = array();
+    foreach($result as $row){
+        $t_map[$row['auto_id']] = $row['name'];
+    }
 
     $type = NULL;
     if($t_query == '%'){
         $type = 'Any';
     }else{
         $t_wild = false;
-        $sql = "SELECT `name` FROM `type` WHERE `auto_id` = '$t_query'";
-        $result = $db->query($sql) or 
-            die("Something went wrong with $sql<br>".$db->error);
-
-        while($data = $result->fetch_array(MYSQLI_ASSOC))
-        {
-            $type = $data['name'];
-        }
+        $type = $t_map[$t_query];
     }
 
     if(!$t_wild && !$m_wild){
         $sql = "SELECT * FROM `equipment` WHERE `type` = '$t_query' AND `manufacture` = '$m_query' AND `serial_number` LIKE '%$s_query%' LIMIT $n_query";
+        $result = $db->query($sql) or 
+            die("Something went wrong with $sql<br>".$db->error);
+        
+            echo '<h3>Search by manufacture '.$manufacture.' type '.$type.' showing '.$n_query.' results.</h3>';
+            echo '<table>';
+        echo '<tr><th>Auto ID</th><th>Serial Number</th></tr>';
+        while($data=$result->fetch_array(MYSQLI_ASSOC)){
+            echo '<tr>';
+                echo '<td>'.$data['auto_id'].'</td>';
+                echo '<td>'.$data['serial_number'].'</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
     }elseif (!$t_wild && $m_wild) {
         $sql = "SELECT * FROM `equipment` WHERE `type` = '$t_query' AND `serial_number` LIKE '%$s_query%' LIMIT $n_query";
+        $result = $db->query($sql) or 
+            die("Something went wrong with $sql<br>".$db->error);
+        
+            echo '<h3>Search by type '.$type.' showing '.$n_query.' results.</h3>';
+            echo '<table>';
+        echo '<tr><th>Auto ID</th><th>Type</th><th>Manufacture</th><th>Serial Number</th></tr>';
+        while($data=$result->fetch_array(MYSQLI_ASSOC)){
+            echo '<tr>';
+                echo '<td>'.$data['auto_id'].'</td>';
+                echo '<td>'.$m_map[$data['manufacture']].'</td>';
+                echo '<td>'.$data['serial_number'].'</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
     }elseif ($t_wild && !$m_wild) {
         $sql = "SELECT * FROM `equipment` WHERE `manufacture` = '$m_query' AND `serial_number` LIKE '%$s_query%' LIMIT $n_query";
+        $result = $db->query($sql) or 
+            die("Something went wrong with $sql<br>".$db->error);
+        
+            echo '<h3>Search by manufacture '.$manufacture.' showing '.$n_query.' results.</h3>';
+            echo '<table>';
+        echo '<tr><th>Auto ID</th><th>Type</th><th>Manufacture</th><th>Serial Number</th></tr>';
+        while($data=$result->fetch_array(MYSQLI_ASSOC)){
+            echo '<tr>';
+                echo '<td>'.$data['auto_id'].'</td>';
+                echo '<td>'.$t_map[$data['type']].'</td>';
+                echo '<td>'.$data['serial_number'].'</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
     }elseif ($t_wild && $m_wild) {
         $sql = "SELECT * FROM `equipment` WHERE `serial_number` LIKE '%$s_query%' LIMIT $n_query";
+        $result = $db->query($sql) or 
+            die("Something went wrong with $sql<br>".$db->error);
+        
+            echo '<h3>Search by manufacture '.$manufacture.' type '.$type.' showing '.$n_query.' results.</h3>';
+            echo '<table>';
+        echo '<tr><th>Auto ID</th><th>Type</th><th>Manufacture</th><th>Serial Number</th></tr>';
+        while($data=$result->fetch_array(MYSQLI_ASSOC)){
+            echo '<tr>';
+                echo '<td>'.$data['auto_id'].'</td>';
+                echo '<td>'.$t_map[$data['type']].'</td>';
+                echo '<td>'.$m_map[$data['manufacture']].'</td>';
+                echo '<td>'.$data['serial_number'].'</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
     }
-
-    $result = $db->query($sql) or 
-        die("Something went wrong with $sql<br>".$db->error);
-
-    echo '<h3>Search by manufacture '.$manufacture.' type '.$type.' showing '.$n_query.' results.</h3>';
-    echo '<table>';
-    echo '<tr><th>Type</th><th>Manufacture</th><th>Serial Number</th></tr>';
-    while($data=$result->fetch_array(MYSQLI_ASSOC)){
-        echo '<tr>';
-            echo '<td>'.$data['type'].'</td>';
-            echo '<td>'.$data['manufacture'].'</td>';
-            echo '<td>'.$data['serial_number'].'</td>';
-        echo '</tr>';
-    }
-    echo '</table>';
 
 
     $time_end = microtime(true);
