ad2d5cd8021e91e47ced8d98974aa5f7e2d89df8
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-21 20:46:03 -0500

    Updated Modify Pages

--- modify_type.php
+++ modify_type.php
@@ -5,7 +5,8 @@
         $t_query = $_POST['type'];
         $nt_query = $_POST['new_type'];
 
-        // $db = db_iconnect('warehouse_works');
+        $db = db_iconnect('warehouse-works');
+        $sql = "UPDATE `type` SET `name` = '$nt_query' WHERE `auto_id` = $t_query";
         // $sql = "UPDATE `type` SET `name` = '$nt_query' WHERE `auto_id` = '$t_query'";
         // // $db->query($sql) or 
         // //     die("Something went wrong with $sql<br>".$db->error);
