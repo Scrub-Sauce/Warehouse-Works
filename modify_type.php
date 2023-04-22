583d566674b310089855807d75f5615a7c24a4b3
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-21 20:28:01 -0500

    Added Modify Pages

---
+++ modify_type.php
@@ -0,0 +1,36 @@
+<?php
+    include 'db_connect';
+
+    if(isset($_POST['submit']) && ($_POST['submit'] == 'submit')){
+        $t_query = $_POST['type'];
+        $nt_query = $_POST['new_type'];
+
+        $db = db_connect('warehouse_works');
+        $sql = "UPDATE `equipment` SET `name` = '$nt_query' WHERE `auto_id` = '$t_query'";
+        $db->query($sql) or 
+            die("Something went wrong with $sql<br>".$db->error);
+        echo "<h2>type has been changed to $nt_query</h2>";
+
+
+    } else {
+        echo '<h1>Modify Type</h1>';
+        echo '<form method="post" action="">';
+            echo '<label for="type"></label>';
+            echo '<select name="type" id="type">';
+            $db = db_iconnect('warehouse-works');
+            $sql = "SELECT * FROM `type`";
+            $result = $db->query($sql) or 
+                die ("Something went wrong with $sql<br>".$db->error);
+            while($data = $results->fetch_array(MYSQLI_ASSOC){
+                echo '<option value="'.$data['auto_id'].'">'.$data['name'].'</option>';
+            })
+            echo '</select>';
+            echo '<label for="new_type">New Type: </label>';
+            echo '<input type="text" name="new_type">';
+            echo '<button type="submit" name="submit" value="submit">Submit</button>';
+        echo '</form>';
+    }
+
+    
+    
+?>
