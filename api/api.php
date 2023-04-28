9742892b7ee7a31ddaaccd447eb8dd24a06c505c
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-28 17:05:31 -0500

    Updated api.php

--- api/api.php
+++ api/api.php
@@ -1,2 +1,11 @@
 <?php
-    echo '<h2>API Page</h2>';
+header('Content-Type: application/json');
+header('HTTP/1.1 200 OK');
+
+$output[] = 'Status: API Main';
+$output[] = 'MSG: Primary Endpoint Reached';
+$output[] = 'Action: none';
+
+$responseData = json_encode($output);
+
+echo $responseData;
