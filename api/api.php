437dbe6229f597f6f1d23e9573b9386808d0ad78
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-29 14:28:51 -0500

    Added api/seach.php

--- api/api.php
+++ api/api.php
@@ -1,37 +1,16 @@
 <?php
 header('Content-Type: application/json');
 header('HTTP/1.1 200 OK');
-//
-//$output[] = 'Status: API Main';
-//$output[] = 'MSG: Primary Endpoint Reached';
-//$output[] = 'Action: none';
-//
-//$responseData = json_encode($output);
-//
-//echo $responseData;
 
 $url = $_SERVER['REQUEST_URI'];
-//echo "<h3>$url</h3>";
-
 $path = parse_url($url, PHP_URL_PATH);
-//echo "<h3>$path</h3>";
-
 $pathComponents = explode("/", trim($path,"/"));
-//echo '<h3>Number of URL components: '.count($pathComponents).'</h3>';
-
-//echo "<pre>";
-//print_r($pathComponents);
-//echo "</pre>";
 
 $endPoint = $pathComponents[1];
 
 switch($endPoint) {
-    case "search_manufacture":
-        $output[] = 'Status: Success';
-        $output[] = 'MSG: Manufacture Endpoint Reached';
-        $output[] = 'Action: None';
-        $responseData = json_encode($output);
-        echo $responseData;
+    case "search":
+        include("search.php");
         break;
 
     default:
---
+++ api/search.php
@@ -0,0 +1,79 @@
+<?php
+include ("../db_connect.php");
+
+$db = db_iconnect('warehouse-works');
+
+$m_query = $_REQUEST['manufacture'];
+$t_query = $_REQUEST['type'];
+$n_query = $_REQUEST['num'];
+$s_query = $_REQUEST['serial_num'];
+$stat_query = $_REQUEST['status'];
+$info[] = array();
+
+// Create a map of all manufacturers so we don't need to make additional queries.
+$sql = "SELECT `auto_id`, `name` FROM `manufacture`";
+$result = $db->query($sql) or
+die("Something went wrong with $sql<br>".$db->error);
+$m_map = array();
+foreach($result as $row){
+    $m_map[$row['auto_id']] = $row['name'];
+}
+
+// Check value of Manufacture
+if($m_query == 'all'){
+    $manufacture = "`manufacture` LIKE '%'";
+}else{
+    $manufacture = "`manufacture` = '$m_query'";
+}
+
+// Create a map of all types so we don't need unnecessary queries.
+$sql = "SELECT `auto_id`, `name` FROM `type`";
+$result = $db->query($sql) or
+die("Something went wrong with $sql<br>".$db->error);
+$t_map = array();
+foreach($result as $row){
+    $t_map[$row['auto_id']] = $row['name'];
+}
+
+// Check Value of type
+if($t_query == 'all'){
+    $type = "`type` LIKE '%'";
+}else{
+    $type = "`type` = '$t_query'";
+}
+
+// Create a map of all Statuses so we don't need unnecessary queries.
+$sql = "SELECT * FROM `status`";
+$result = $db->query($sql) or
+die("Something went wrong with $sql<br>".$db->error);
+$stat_map = array();
+foreach($result as $row){
+    $stat_map[$row['auto_id']] = $row['name'];
+}
+
+// Check value of status
+if($s_query == 'all'){
+    $status = "`status` LIKE '%'";
+}else{
+    $status = "`status` = '$stat_query";
+}
+
+$sql = "SELECT * FROM `equipment` WHERE $type AND $manufacture AND $status AND `serial_number` LIKE '%$s_query%' LIMIT $n_query";
+$result = $db->query($sql) or
+    die('Something went wrong with $sql'.$db->error);
+while($data = $result->fetch_array(MYSQLI_ASSOC)){
+    $info[] = array($data['auto_id'], $t_map[$data['type']], $m_map[$data['manufacture']], $data['serial_number'], $stat_map[$data['status']]);
+}
+
+$time_end = microtime(true);
+$seconds = $time_end - $time_start;
+$execution_time = ($seconds) / 60;
+
+$output[] = "Status: Success";
+$output[] = "MSG: $info";
+$output[] = "Action: $execution_time";
+$responseData = json_encode($output);
+echo "$responseData";
+
+echo "<p>Execution time: $execution_time minutes or $seconds seconds. </p>";
+echo '<a href="./search.php">Home</a>';
