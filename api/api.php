3b2455f08b803818a8139ff4fa742d2c91fb6a78
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-28 22:22:44 -0500

    Updated api.php

--- api/api.php
+++ api/api.php
@@ -17,4 +17,16 @@ $path = parse_url($url, PHP_URL_PATH);
 echo "<h3>$path</h3>";
 
 $pathComponents = explode("/", trim($path,"/"));
-echo '<h3>Number of URL components:'.count($pathComponents).'</h3>';
+echo '<h3>Number of URL components: '.count($pathComponents).'</h3>';
+
+echo "<pre>";
+print_r($pathComponents);
+echo "</pre>";
+
+$endPoint = $pathComponents[1];
+
+switch($endPoint) {
+    case "search_manufacturer":
+        echo "<h3>Load Manufactures Endpoint";
+        break;
+}
