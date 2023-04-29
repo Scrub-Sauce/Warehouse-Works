b8e5a0be7b0976cb2631e41d0c9a66f3860d324f
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-28 22:30:57 -0500

    Updated api.php

--- api/api.php
+++ api/api.php
@@ -1,6 +1,6 @@
 <?php
-//header('Content-Type: application/json');
-//header('HTTP/1.1 200 OK');
+header('Content-Type: application/json');
+header('HTTP/1.1 200 OK');
 //
 //$output[] = 'Status: API Main';
 //$output[] = 'MSG: Primary Endpoint Reached';
@@ -11,24 +11,33 @@
 //echo $responseData;
 
 $url = $_SERVER['REQUEST_URI'];
-echo "<h3>$url</h3>";
+//echo "<h3>$url</h3>";
 
 $path = parse_url($url, PHP_URL_PATH);
-echo "<h3>$path</h3>";
+//echo "<h3>$path</h3>";
 
 $pathComponents = explode("/", trim($path,"/"));
-echo '<h3>Number of URL components: '.count($pathComponents).'</h3>';
+//echo '<h3>Number of URL components: '.count($pathComponents).'</h3>';
 
-echo "<pre>";
-print_r($pathComponents);
-echo "</pre>";
+//echo "<pre>";
+//print_r($pathComponents);
+//echo "</pre>";
 
 $endPoint = $pathComponents[1];
 
 switch($endPoint) {
     case "search_manufacture":
-        echo "<h3>Load Manufactures Endpoint";
+        $output[] = 'Status: Success';
+        $output[] = 'MSG: Manufacture Endpoint Reached';
+        $output[] = 'Action: None';
+        $responseData = json_encode($output);
+        echo $responseData;
         break;
+
     default:
-        echo "<h3>Invalid endpoint: $endPoint</h3>";
+        $output[] = 'Status: Error';
+        $output[] = "MSG: API Endpoint $endPoint not found";
+        $output[] = 'Action: None';
+        $responseData = json_encode($output);
+        echo $responseData;
 }
