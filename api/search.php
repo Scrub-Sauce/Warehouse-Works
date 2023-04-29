5eea959f3ce4fc0417411f1b809c06e6ee03cf16
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-29 15:27:52 -0500

    Updated api/seach.php

--- api/search.php
+++ api/search.php
@@ -3,11 +3,51 @@ include ('db_connect.php');
 
 $db = db_iconnect('warehouse-works');
 
-$m_query = $_REQUEST['manufacture'];
-$t_query = $_REQUEST['type'];
-$n_query = $_REQUEST['num'];
+if(!isset($_REQUEST['type'])){
+    $output[] = "Status: Error";
+    $output[] = "MSG: Type data NULL";
+    $output[] = "Action: Resend Type data";
+    $responseData = json_encode($output);
+    echo "$responseData";
+    die();
+}else{
+    $t_query = $_REQUEST['type'];
+}
+
+if(!isset($_REQUEST['manufacture'])){
+    $output[] = "Status: Error";
+    $output[] = "MSG: Manufacture data NULL";
+    $output[] = "Action: Resend Manufacture data";
+    $responseData = json_encode($output);
+    echo "$responseData";
+    die();
+}else{
+    $m_query = $_REQUEST['manufacture'];
+}
+
+if(!isset($_REQUEST['status'])){
+    $output[] = "Status: Error";
+    $output[] = "MSG: Status data NULL";
+    $output[] = "Action: Resend Status data";
+    $responseData = json_encode($output);
+    echo "$responseData";
+    die();
+}else{
+    $stat_query = $_REQUEST['status'];
+}
+if(!isset($_REQUEST['num'])){
+    $output[] = "Status: Error";
+    $output[] = "MSG: Number of Results data NULL";
+    $output[] = "Action: Resend Num data";
+    $responseData = json_encode($output);
+    echo "$responseData";
+    die();
+}else{
+    $n_query = $_REQUEST['num'];
+}
+
 $s_query = $_REQUEST['serial_num'];
-$stat_query = $_REQUEST['status'];
+
 $info[] = array();
 
 $time_start = microtime(true);
@@ -24,7 +64,8 @@ foreach($result as $row){
 if($m_query == 'all'){
     $manufacture = "`manufacture` LIKE '%'";
 }else{
-    $manufacture = "`manufacture` = '$m_query'";
+    $key = array_search($m_query, $m_map);
+    $manufacture = "`manufacture` = '$key'";
 }
 
 // Create a map of all types so we don't need unnecessary queries.
@@ -40,7 +81,8 @@ foreach($result as $row){
 if($t_query == 'all'){
     $type = "`type` LIKE '%'";
 }else{
-    $type = "`type` = '$t_query'";
+    $key = array_search($t_query, $t_map);
+    $type = "`type` = '$key'";
 }
 
 // Create a map of all Statuses so we don't need unnecessary queries.
@@ -56,26 +98,25 @@ foreach($result as $row){
 if($s_query == 'all'){
     $status = "`status` LIKE '%'";
 }else{
-    $status = "`status` = '$stat_query'";
+    $key = array_search($stat_query, $stat_map);
+    $status = "`status` = '$key'";
 }
 
-$sql = "SELECT * FROM `equipment` WHERE $type AND $manufacture AND $status AND `serial_number` LIKE '%$s_query%' LIMIT 10";
-echo("$sql");
+// Form query and run it
+$sql = "SELECT * FROM `equipment` WHERE $type AND $manufacture AND $status AND `serial_number` LIKE '%$s_query%' LIMIT $n_query";
 $result = $db->query($sql) or
     die('Something went wrong with $sql'.$db->error);
 while($data = $result->fetch_array(MYSQLI_ASSOC)){
-    $info[] = array($data['auto_id'], $t_map[$data['type']], $m_map[$data['manufacture']], $data['serial_number'], $stat_map[$data['status']]);
+    $info[] = array($t_map[$data['type']], $m_map[$data['manufacture']], $data['serial_number'], $stat_map[$data['status']]);
 }
+$infoData = json_encode($info);
 
 $time_end = microtime(true);
 $seconds = $time_end - $time_start;
 $execution_time = ($seconds) / 60;
 
 $output[] = "Status: Success";
-$output[] = "MSG: $info";
+$output[] = "MSG: $infoData";
 $output[] = "Action: $execution_time";
 $responseData = json_encode($output);
 echo "$responseData";
-
-echo "<p>Execution time: $execution_time minutes or $seconds seconds. </p>";
-echo '<a href="./search.php">Home</a>';
