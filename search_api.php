19c3408d934b4c91da50fa53a48d069a44fcedc0
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-29 23:06:34 -0500

    Updated search_api.php

--- search_api.php
+++ search_api.php
@@ -9,11 +9,10 @@ echo '<head>';
     echo '<meta charset="UTF-8">';
     echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
     echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-    echo '<link rel="stylesheet" href="main.css">';
+    echo '<link rel="stylesheet" href="./main.css">';
     echo '<title>Search</title>';
 echo '</head>';
 echo '<body>';
-
     echo '<h2>Search Database</h2>';
     echo '<div class="form-wrapper">';
         echo '<form method="post" action="">';
