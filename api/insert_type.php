87fe5172367af52f618cf2ae229ebeb0ab2a5da5
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-30 15:09:18 -0500

    Added insert_type.php, insert_manufacture.php, insert_equipment.

--- api/insert_type.php
+++ api/insert_type.php
@@ -10,7 +10,9 @@ if(!isset($_REQUEST['type'])){
     die();
 }
 
-$type = $_POST['type'];
+$type = $_REQUEST['type'];
+
+echo "$type";
 
 $time_start = microtime(true);
 $db = db_iconnect('warehouse-works');
@@ -19,25 +21,25 @@ $sql = "SELECT * FROM `type` WHERE `name` ='$type'";
 $result = $db->query($sql);
 $data = $result->fetch_array(MYSQLI_ASSOC);
 
-if($data == NULL) {
-    $sql = "INSERT INTO `type` (`name`) VALUES ('$type')";
-    $result= $db->query($sql) or
-        die ("Something went wrong with $sql<br>".$db->error);
-
-    $time_end = microtime(true);
-    $seconds = $time_end - $time_start;
-    $execution_time = ($seconds) / 60;
-
-    $output[] = "Status: Error";
-    $output[] = "MSG: $type added successfully";
-    $output[] = "Action: $execution_time";
-    $responseData = json_encode($output);
-    echo "$responseData";
-} else {
-    $output[] = "Status: Error";
-    $output[] = "MSG: Provided type already exists";
-    $output[] = "Action: None";
-    $responseData = json_encode($output);
-    echo "$responseData";
-    die();
-}
+//if($data == NULL) {
+//    $sql = "INSERT INTO `type` (`name`) VALUES ('$type')";
+//    $result= $db->query($sql) or
+//        die ("Something went wrong with $sql<br>".$db->error);
+//
+//    $time_end = microtime(true);
+//    $seconds = $time_end - $time_start;
+//    $execution_time = ($seconds) / 60;
+//
+//    $output[] = "Status: Error";
+//    $output[] = "MSG: $type added successfully";
+//    $output[] = "Action: $execution_time";
+//    $responseData = json_encode($output);
+//    echo "$responseData";
+//} else {
+//    $output[] = "Status: Error";
+//    $output[] = "MSG: Provided type already exists";
+//    $output[] = "Action: None";
+//    $responseData = json_encode($output);
+//    echo "$responseData";
+//    die();
+//}
