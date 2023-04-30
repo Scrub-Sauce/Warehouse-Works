4358fb9f87cc8947939fc48aff4689124669b0e2
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-30 15:13:14 -0500

    Updated insert_type.php

--- api/insert_type.php
+++ api/insert_type.php
@@ -20,7 +20,7 @@ $result = $db->query($sql) or
     die("Something went wrong with $sql".$db->error);
 $data = $result->fetch_array(MYSQLI_ASSOC);
 
-echo "$data";
+echo "'$data'";
 
 //if($data == NULL) {
 //    $sql = "INSERT INTO `type` (`name`) VALUES ('$type')";
