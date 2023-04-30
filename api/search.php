c5a3a046a591b70576883e6d5604adb722db3ec3
Kyle Anderson <kyleandersontx@gmail.com>
2023-04-29 19:09:26 -0500

    Added search_api.php

--- api/search.php
+++ api/search.php
@@ -106,7 +106,8 @@ $sql = "SELECT * FROM `equipment` WHERE $type AND $manufacture AND $status AND `
 $result = $db->query($sql) or
     die('Something went wrong with $sql'.$db->error);
 while($data = $result->fetch_array(MYSQLI_ASSOC)){
-    $info[] = array($t_map[$data['type']], $m_map[$data['manufacture']], trim($data['serial_number']), $stat_map[$data['status']]);
+    $serial_number = trim($data['serial_number']);
+    $info[] = array($t_map[$data['type']], $m_map[$data['manufacture']], $serial_number, $stat_map[$data['status']]);
 }
 $infoData = json_encode($info);
 
